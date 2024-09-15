7c4044787a2177122af708588363c5a73a68e602
Abdinasir Dahir <abdinasir.dahir@moringaschool.com>
2024-09-15 23:26:42 +0300

    completed lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,28 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"Invalid pet type. Must be one of {self.PET_TYPES}")
+        self.pet_type = pet_type
+        self.owner = owner  # We add this line to create the owner attribute
+        Pet.all.append(self)
+    
+    
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Pet must be an instance of Pet class")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
